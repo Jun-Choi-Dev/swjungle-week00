a934f61dada17fc78c8ff0fd494213bee66c781d
dkkim0122 <dkkim0122@gmail.com>
2021-11-02 20:12:28 +0900

    로그인 시 user db 대조하는 로직 추가

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import name
 from flask import Flask
 from flask import jsonify
 from flask import request
@@ -16,6 +17,35 @@ db = client.week00
 
 app.config["JWT_SECRET_KEY"] = "week00_team_6"
 jwt = JWTManager(app)
+
+@app.route("/register", methods=["POST"])
+def register():
+    user_id = request.json.get("user_id")
+    password = request.json.get("password")
+    name = request.json.get("name")
+
+    db.userdata.insert_one({'user_id':user_id, 'password':password, 'name':name,
+    'bike_number' : None, 'penalty_score' : 0})
+
+    return jsonify({"result": "success"})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    user_id = request.json.get("user_id", None)
+    password = request.json.get("password", None)
+
+    # userdata(db) 대조 로직 추가
+    user = db.userdata.find_one({'user_id':user_id})
+    if user != None:
+        if user['password'] == password:
+            access_token = create_access_token(identity=user_id)
+            return jsonify(access_token=access_token)
+        else:
+            return jsonify({'result': "warning : 비밀번호가 맞지 않습니다."})
+    else:
+        return jsonify({'result': "warning : 회원가입이 필요합니다."})
+
 
 # API를 여기 아래서부터 만들어주세요.
 
